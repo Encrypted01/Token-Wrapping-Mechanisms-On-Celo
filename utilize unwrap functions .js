a58cb9a7887e89f940bb0d406edb65c98101fa87
Encrypted <132623893+Encrypted01@users.noreply.github.com>
2023-06-20 06:57:54 +0100

    utilize unwrap functions .js

---
+++ utilize unwrap functions .js
@@ -0,0 +1,5 @@
+// Unwrap tokens
+const amountToUnwrap = '500000000000000000'; // Amount of wrapped tokens to unwrap
+
+await unwrapTokens(tokenAddress, amountToUnwrap);
+console.log(`Tokens unwrapped successfully!`);
